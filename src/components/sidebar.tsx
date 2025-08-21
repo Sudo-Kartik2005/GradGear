4071c88d10ab91220b6759bd1a4618b6ff54a5da
Kartik <kartik829909@gmail.com>
2025-08-21 10:38:28 +0000

    add logo in it

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,7 +3,7 @@
 
 import Link from "next/link"
 import {
-  Laptop,
+  GraduationCap,
   Home,
   Star,
   LineChart,
@@ -27,7 +27,7 @@ export function Sidebar() {
             href="/"
             className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
           >
-            <Laptop className="h-4 w-4 transition-all group-hover:scale-110" />
+            <GraduationCap className="h-5 w-5 transition-all group-hover:scale-110" />
             <span className="sr-only">GradGear</span>
           </Link>
           <Tooltip>
